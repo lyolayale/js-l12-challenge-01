f386bcd44b1ddc6af8f4d2d1721d21ac0158b6c9
Eric McKee <lyolayale19@gmail.com>
2022-11-03 10:31:27 -0500

    Created async function to fetch 100 api objects & created random img --> dom createElemnt

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
+/*
+- Purpose: Practice JS API fetch --> Image Generator
+- Date: 03-NOV-2022
+*/
+
+// ==== variables ====
 const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+// ==== async functions ====
+const getImage = async function (
+  url = "https://picsum.photos/v2/list?limit=100"
+) {
+  const res = await fetch(url);
+  const images = await res.json();
+  const randomImage = images[Math.floor(Math.random() * images.length)];
+  const img2 = document.createElement("img");
+  img2.src = randomImage.download_url;
+
+  document.body.prepend(img2);
+  console.log(randomImage);
+  console.log(images);
+};
+
+getImage();
